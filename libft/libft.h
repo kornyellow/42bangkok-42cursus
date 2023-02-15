a48be99a7e61d5987c3dae4298813a4cf1cff8e5
kornyellow <kornkubzaza@gmail.com>
2023-02-16 03:16:12 +0700

    Working on libft

---
+++ libft/libft.h
@@ -0,0 +1,13 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+int ft_isalpha(char c);
+int ft_isdigit(char c);
+int ft_isalnum(char c);
+int ft_isascii(char c);
+int ft_isprint(char c);
+
+#endif
